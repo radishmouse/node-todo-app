3d9edc24206aa5a7db58acd8ca87aa093d051de0
Chris Aquino <chris.aquino@gmail.com>
2018-11-05 11:42:16 -0500

    converts example sql call to function

--- index.js
+++ index.js
@@ -8,7 +8,38 @@ const db = pgp({
     database: 'node-todo-app-db'
 });
 
-db.any('select * from todos')
-    .then(results => {
-        console.log(results);
+// example of grabbing all the rows
+// db.any('select * from todos')
+//     .then(results => {
+//         console.log(results);
+//     })
+
+// example of grabbing one row
+function getById(id){
+    return db.one(`select * from todos where id = ${id}`)
+        .catch(err => {
+            // Got nuthin'
+            // console.log('you did not get a todo');
+            return {
+                name: 'No todo found.'
+            };   
+        })
+}
+
+getById(2)
+    .then(result => {
+        console.log(result);
     })
+
+getById(2000000)
+    .then(result => {
+        console.log(result);
+    })
+
+
+
+// example of adding a row
+
+// example of updating a row
+
+// example of deleting a row
