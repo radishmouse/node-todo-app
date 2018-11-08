296002dc2a3ef021e85ce458368bb9aa9c97add0
Chris Aquino <chris.aquino@gmail.com>
2018-11-08 11:10:17 -0500

    sends all users back as JSON

--- index.js
+++ index.js
@@ -8,9 +8,46 @@ const User = require('./models/User');
 
 // Listen for a GET request
 app.get('/', (req, res) => {
-    res.send('Hellooooooo Expresssssssssssssuh');
+    User.getAll()
+        .then(allUsers => {
+            // res.status(200).json(allUsers);
+            res.send(allUsers);
+        })
 });
 
 app.listen(3000, () => {
     console.log('You express app is ready!');
 });
+
+
+
+// ===== example of sending a whole page
+
+/*
+    User.getAll()
+        .then(allUsers => {
+            let usersList = ``;
+            allUsers.forEach(user => {
+                usersList += `<li>${user.name}</li>`
+            });
+            let thePage = `
+              <!doctype>
+              <html>
+                <head>
+                </head>
+                <body>
+                    <h1>hey</h1>
+                    <ul>
+                        ${usersList}
+                    </ul>
+                </body>
+              </html>
+            `;
+            res.send(thePage);
+            // console.log(allUsers);
+            // res.send(allUsers);
+            // res.send(allUsers);
+            // res.status(200).json(allUsers);
+        })
+    // res.send('Hellooooooo Expresssssssssssssuh');
+*/
