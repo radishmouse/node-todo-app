03d585b0cf4e73acc611acdcd6b0dce574fa8f0f
Chris Aquino <chris.aquino@gmail.com>
2019-01-14 14:36:41 -0500

    adds react build files

---
+++ public/precache-manifest.1cb3cb3407b74f95a7414d88f473a204.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c4714c716a5448b4938",
+    "url": "/static/js/main.3c4714c7.chunk.js"
+  },
+  {
+    "revision": "e8e601dfa9e56a556515",
+    "url": "/static/js/1.e8e601df.chunk.js"
+  },
+  {
+    "revision": "3c4714c716a5448b4938",
+    "url": "/static/css/main.5d67f594.chunk.css"
+  },
+  {
+    "revision": "f86516bfa65c03e314385eb125aede81",
+    "url": "/index.html"
+  }
+];
